c7b0683d586b7d26952fe066c32702501ccadf99
Afganli97 <ulviva@code.edu.az>
2023-01-23 22:24:20 +0400

    Create register form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,9 +12,8 @@ export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
   ngOnInit() {
-    this.getUSers();
     this.setCurrentUser();
     }
 
@@ -23,11 +22,4 @@ export class AppComponent implements OnInit {
       this.accountService.setCurrentUser(user);
     }
 
-    getUSers(){
-      this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    } 
-    )}
 }
